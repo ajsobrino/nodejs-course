3406a40392657f65e613a96e225080e8fcf7ed3c
Sobrino Bautista <ajsobrino@minsait.com>
2020-04-27 23:00:17 +0200

    Add some config and to work with body

--- jwtAuth/app.js
+++ jwtAuth/app.js
@@ -1,13 +1,27 @@
+require('./config/config');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/user', (req, res) => {
-    res.json("GET");
+app.get('/user/:id', (req, res) => {
+    let id = req.params.id;
+    res.json("GET " + id);
 })
 
 app.post('/user', (req, res) => {
-    res.json('Post');
+    let body = req.body;
+    if (body.name == undefined) {
+        res.status(400).json({
+            message: 'The name is neccesary',
+            ok: false
+        })
+    }
+    res.json({
+        method: 'Post ',
+        body: body
+    });
 });
 
 app.put('/user', (req, res) => {
@@ -19,4 +33,6 @@ app.delete('/user', (req, res) => {
 });
 
 
-app.listen(3000);
+app.listen(process.env.PORT, () => {
+    console.log('The app is listening in ' + process.env.PORT);
+});
---
+++ jwtAuth/config/config.js
@@ -0,0 +1,4 @@
+process.env.PORT = process.env.PORT || 3000;
+
+
+module.exports = process.env.PORT;
